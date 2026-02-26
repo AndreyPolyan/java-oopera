1347f54753a9c47fd5cc551ce69677ba53681f4d
aipolyanskiy <andy3101@yandex.ru>
2026-02-26 18:14:10 +0300

    MainCommit- FixingTypos

--- src/ru/practicum/yandex/model/workers/Choreographer.java
+++ src/ru/practicum/yandex/model/workers/Choreographer.java
@@ -1,7 +1,6 @@
 package ru.practicum.yandex.model.workers;
 
-public class Choreographer extends Person{
-
+public class Choreographer extends Person {
     public Choreographer(Gender gender, String name, String surname) {
         super(gender, name, surname);
     }
